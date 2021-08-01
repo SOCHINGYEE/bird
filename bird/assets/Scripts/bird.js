451aff00ba0fd8ba145244d3ee0663e4c6452274
SOCHINGYEE <sochingyee@gmail.com>
2021-08-01 19:56:54 +0800

    小鸟旋转动画

--- bird/assets/Scripts/bird.js
+++ bird/assets/Scripts/bird.js
@@ -16,22 +16,39 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        //cc.move(时间间隔，x, y)
-        let moveTo = cc.moveTo(2,500,1000);
-        let func = cc.callFunc(this.print,this)
-        let seq = cc.sequence(moveTo,func)
-      
-        this.node.runAction(seq)
+
+        this.animation = this.getComponent('cc.Animation')
+        this.state = 'none' //记录旋转角度的状态
         
     },
-
+   
     start () {
 
     },
-    print() {
-        console.log('这是回调函数')
+    //切换状态
+    switchState(state) {
+        if(this.state === state) {
+            //如果当前的状态等于要切换的状态
+            return
+        }
+        this.state = state //切换状态
+        this.node.stopAllActions() //停止动作
+        this.animation.stop() //停止动画
+        //需要切换成上升状态成功，播放相应的动画
+        if (this.state === 'up') {
+            this.animation.play('wings') 
+            //播放旋转动作
+            let rotateTo = cc.rotateTo(0.3,-60)
+            this.node.runAction(rotateTo)
+        }else if (this.state === 'drop') {
+            let rotateTo = cc.rotateTo(0.3,60)
+            this.node.runAction(rotateTo)
+        }
     },
+
+
     up() {
+        this.switchState('up')
         this.speed = 500 //给一个正数，相当于给一个向上的速度，速度减为0的时候才会做向下自由落体运动
     },
     update (dt) {
@@ -41,5 +58,10 @@ cc.Class({
         this.speed += this.accel*dt
         // //位移随时间变化的规律 h=1/2gt^2=1/2*accel*dt*dt =this.speed*dt
         this.node.y += this.speed *dt
+        console.log('打印速度',this.speed)
+        if (this.speed <= 0 ) {
+            //速度降落到0，开始下落
+            this.switchState('drop')
+        }
     },
 });
